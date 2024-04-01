91ffdd0b2c9242660a7acef4b1019d0712c5fb70
aero@lhcpheno32 <andres.renteria@ific.uv.es>
2024-04-01 14:38:24 +0200

    Adding the 4x12_equals_blok to fix the qmk_firmware repo

---
+++ 4x12_equals_blok/rules.mk
@@ -0,0 +1,15 @@
+# This file intentionally left blank
+#
+
+# 2024.01.22 : modification by Andres Renteria
+# Enabling OS detection
+OS_DETECTION_ENABLE = yes
+
+# Backlight
+BACKLIGHT_ENABLE = yes
+
+RGB_MATRIX_ENABLE = yes
+#RGB_MATRIX_DRIVER = is31fl3731
+
+
+
